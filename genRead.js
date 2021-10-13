84c303c2af6b7a210744e258e262e826a3e38ce0
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-10-13 16:20:33 -0700

    prompts and readme outline have been created

---
+++ genRead.js
@@ -0,0 +1,36 @@
+const genReadMe = () => {
+
+    `# ${this.title}
+    ## Description
+    ${this.description}
+    ## Table of Contents 
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Contact](#contact)
+    ## Installation
+    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    ## Usage
+    ${this.usage}
+    ## Credits
+    ${this.credit}
+    ## License
+    ${this.license}
+    ---
+    ## Features
+    If your project has a lot of features, list them here.
+
+    
+    ## How to Contribute
+  ${this.contribution}
+    ## Tests
+    ${this.tests}
+    ##Contact
+    https://github.com/${this.gitHub}
+    https://www.linkedin.com/in/${this.linkedIn}/
+    Email: ${this.email}
+    
+    `
+}
---
+++ index.js
@@ -0,0 +1,89 @@
+const genRead = require('./genRead')
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+
+
+
+
+function userPromt() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project called?',
+            
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'How is your project installed?',
+            
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe how your project works?',
+            
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is your GitHub URL?',
+            
+        },
+        {
+            type: 'input',
+            name: 'credit',
+            message: 'Are there any contributors to the project?',
+            
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How can others contribute to the project?',
+            
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What are the test instructions if any?',
+            
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'What is your GitHub profile name?',
+            
+        },
+        {
+            type: 'input',
+            name: 'linkedIn',
+            message: 'What is your LinkedIn username?',
+            
+        },
+        {
+            type: 'checkbox',
+            name:'license',
+            message:'What licenses would you like to add?',
+            choice:['Modzilla','MIT', 'GNU']
+            
+            
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message:'What is your email?'
+        }
+        
+    ])
+    .then((ans) => {
+        
+        
+        fs.writeFile('README.md', genRead(ans), (err) => {
+            err ? console.log(err) : console.log('Success!')
+        })
+    })
+}
